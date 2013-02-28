b1764d0f89f351591212c2998b5be10e37868ed3
Colin Lockard <clockard@mills.edu>
2013-02-28 10:59:32 -0800

    feb28-4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,8 +9,10 @@ class MoviesController < ApplicationController
   def index
     if params[:sort]=="title"
       @movies=Movie.order("title").all
+      @hilite_title="hilite"
     elsif params[:sort]=="release_date"
       @movies=Movie.order("release_date")
+      @hilite_release="hilite"
     else
       @movies = Movie.all
     end
