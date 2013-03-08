140073996d6b6ff5efd2e1608e8e093cc8c09828
Colin Lockard <clockard@mills.edu>
2013-03-07 17:27:00 -0800

    March 7-2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,41 +7,54 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #session.clear
+
+    #March 5, HW2: Colin added session hash information:
     #Check to see if params has data. If it is missing one or more items that are
-    #saved in sessions hash, 
+    #saved in sessions hash, pass sessions info into params and reload
     if (params[:ratings]==nil and session[:ratings]!=nil) or (params[:sort]==nil and session[:sort]!=nil)
-    #if (params[:ratings]==nil or params[:sort]==nil) and (session[:ratings] != nil or session[:sort] != nil)
       if params[:ratings]==nil and session[:ratings] != nil
         params[:ratings]=session[:ratings]
       end
       if params[:sort]==nil and session[:sort] !=nil
         params[:sort]=session[:sort]
       end
-      flash.keep
+      flash.keep #preserve flash values
       redirect_to movies_path :ratings=>params[:ratings], :sort=>params[:sort]
     end
+    #*----------*#
 
-    @all_ratings=Movie.get_ratings
-    if params[:ratings] != nil
+    #March 2 HW2, Colin added @all_ratings
+    @all_ratings=Movie.get_ratings #get list of all ratings
+    if params[:ratings] != nil #if user has previously selected certain ratings, add to session and populate checkboxes
       session[:ratings] = params[:ratings]
       @ratings_checked=params[:ratings].keys
-    else
+    else #otherwise, if no value in params(meaning user either hasn't made a selection or has deselected all), check all boxes
       @ratings_checked=@all_ratings
     end
+    #*----------*#
+
+    #March 2 HW2, Colin added @checked_ratings_hash
     @checked_ratings_hash=params[:ratings]
+    #*----------*#
+
+    #Feb 28 HW2, Colin added sorting by title/release date and hiliting:
+    #March 2 HW2, added filtering by ratings:
     if params[:sort]=="title"
       @movies=Movie.where(:rating => @ratings_checked).order("title")
       @hilite_title="hilite"
+      #March 4 added session population:
       session[:sort]=params[:sort]
+      #*---------*#
     elsif params[:sort]=="release_date"
       @movies=Movie.where(:rating => @ratings_checked).order("release_date")
       @hilite_release="hilite"
+      #March 4 added session population:
       session[:sort]=params[:sort]
+      #*---------*#
     else
       @movies = Movie.where(:rating => @ratings_checked)
     end
-
+    #*----------*#
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
+	#March 2 HW2 Colin added:
 	def Movie.get_ratings
 		return ['G','PG','PG-13','R','NC-17']
 	end
+	#************#
 
 end
